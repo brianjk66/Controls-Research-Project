d791e74fdcbf131ad592070a4b71559f3ce83ab8
Brian Kaplan <brian.kaplan@gatech.edu>
2018-03-11 19:25:22 -0400

    Added proper delays for arming and calibrating plus serial printouts to show user when they complete

--- Software/Arduino Code/ESC_Test/ESC_Test.ino
+++ Software/Arduino Code/ESC_Test/ESC_Test.ino
@@ -10,23 +10,31 @@ Servo ESC;
 int speed = 0;
 
 void arm(){
-  digitalWrite(DRIVE_PIN, HIGH);
-  setSpeed(0); //Sets speed variable
-  delay(1000);
+  Serial.print("Arming ESC... ");
+  digitalWrite(DRIVE_PIN, LOW);   // Disconnect ESC from power
+  delay(500);                     // Wait 500ms for ESC to power down
+  setSpeed(0);                    // Set speed to 0
+  digitalWrite(DRIVE_PIN, HIGH);  // Reconnect ESC to power
+  delay(2500);                    // 2.5 second delay for ESC to respond
+  Serial.println("Arming complete");
 }
 
 /* Callibrate ESC's PWM range for first use */
 void callibrate() {
+  Serial.print("Calibrating ESC... ");
   digitalWrite(DRIVE_PIN, LOW);   // Disconnect ESC from power
   delay(500);                     // Wait 500ms
-  setSpeed(1000);            // Request full speed
+  setSpeed(1000);                 // Request full speed
   digitalWrite(DRIVE_PIN, HIGH);  // Reconnect ESC to power
-  delay(5000);                     // Wait 5s
-  setSpeed(0);               // Request 0 speed
+  delay(5000);                    // Wait 5 seconds
+  setSpeed(0);                    // Request 0 speed
+  delay(8000);                    // Wait 8 seconds
+  Serial.println("Calibration complete");
 }
 
 void setSpeed(int input_speed){
-  int us = map(input_speed, 0, 1000, 1000, 2000); //Sets servo positions to different speed
+  //Sets servo positions to different speed
+  int us = map(input_speed, 0, 1000, 1000, 2000);
   ESC.writeMicroseconds(us);
 }
 
